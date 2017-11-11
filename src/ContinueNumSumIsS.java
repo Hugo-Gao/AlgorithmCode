02f6ea1b96c12c2d0a06f8d6fae37e5892de70bb
Hugo <598335769@qq.com>
2017-11-11 10:05:23 +0800

    2017/11/11

---
+++ src/ContinueNumSumIsS.java
@@ -0,0 +1,30 @@
+public class ContinueNumSumIsS
+{
+    public static void main(String[] args)
+    {
+        getSequences(15);
+    }
+
+    static void getSequences(int target)
+    {
+        int start = 1;
+        while (start <= target / 2)
+        {
+            StringBuilder sb = new StringBuilder();
+            int begin = start;
+            int sum = begin;
+            sb.append(begin);
+            while (sum<target)
+            {
+                begin++;
+                sum += begin;
+                sb.append("+").append(begin);
+            }
+            if (sum == target)
+            {
+                System.out.println(sb);
+            }
+            start++;
+        }
+    }
+}
---
+++ src/ReverseWord.java
@@ -0,0 +1,38 @@
+public class ReverseWord
+{
+    public static void main(String[] args)
+    {
+        reverseWord("Student.");
+    }
+
+    static void reverseWord(String s)
+    {
+        char[] str = s.toCharArray();
+        reverse(str, 0, str.length - 1);
+        int end = 0, start = 0;
+        while (end < str.length-1)
+        {
+            if(str[end]!=' ')
+            {
+                end++;
+                continue;
+            }
+            reverse(str, start, end-1);
+            end++;
+            start = end;
+        }
+        System.out.println(str);
+    }
+
+    public static void reverse(char[] str, int begin, int end)
+    {
+        while (begin<end)
+        {
+            char temp = str[begin];
+            str[begin] = str[end];
+            str[end] = temp;
+            begin++;
+            end--;
+        }
+    }
+}
